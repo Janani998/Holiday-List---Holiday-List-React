231ce2d4ec9d620c9ae2466c07106c80b02b6990
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-10-19 21:14:58 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  cities = ["Goa(India)", "Darjeeling(India)", "Lonavala(India)"];
   render() {
+    const cities = ["Goa", "Darjeeling", "Lonavala"];
     return (
       <>
         <ol>
-          {this.cities.map((city, index) => (
+          {cities.map((city, index) => (
             <li key={`location${index + 1}`}>{city}</li>
           ))}
         </ol>
